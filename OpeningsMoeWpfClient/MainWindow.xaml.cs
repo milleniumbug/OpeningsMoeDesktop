df8bc7e758176f77287e5b601c58f264404eeeec
milleniumbug <milleniumbug@protonmail.com>
2018-04-17 09:24:56 -0700

    Replace the WPF's MediaElement with Mpv.WPF

--- OpeningsMoeWpfClient/MainWindow.xaml.cs
+++ OpeningsMoeWpfClient/MainWindow.xaml.cs
@@ -11,23 +11,26 @@ namespace OpeningsMoeWpfClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Mpv.WPF.MpvPlayer openingPlayer;
+
         private PlayerModel model;
 
         public MainWindow()
         {
             InitializeComponent();
+            openingPlayer = new Mpv.WPF.MpvPlayer("lib\\mpv-1.dll");
+            PlayerContainer.Children.Add(openingPlayer);
         }
 
         private async Task NextVideo()
         {
             var moviePath = await model.RequestNextMovie();
-            OpeningPlayer.SetCurrentValue(
-                MediaElement.SourceProperty,
-                new Uri(
-                    Path.Combine(
-                        Directory.GetCurrentDirectory(),
-                        moviePath)));
-            OpeningPlayer.Play();
+            openingPlayer.Load(new Uri(
+                Path.Combine(
+                    Directory.GetCurrentDirectory(),
+                    moviePath)).AbsolutePath,
+                Mpv.WPF.LoadMethod.Replace);
+            openingPlayer.Resume();
         }
 
         private async void OnVideoFinishedPlaying(object sender, RoutedEventArgs e)
@@ -37,19 +40,11 @@ namespace OpeningsMoeWpfClient
 
         private async void OnLoaded(object sender, RoutedEventArgs e)
         {
-            
-
-            var ffmpegPath = FfmpegMovieConverter.TryLookupFfmpeg();
-            if (ffmpegPath == null)
-                throw new InvalidOperationException("FFMPEG NOT FOUND");
             var targetDirectory = new DirectoryInfo("Openings");
             model = await PlayerModel.Create(
                 new Random(),
-                new ConvertedMovieProvider(
-                    new MovieDownloader(
-                        new Uri("http://openings.moe/"),
-                        targetDirectory),
-                    new FfmpegMovieConverter(ffmpegPath),
+                new MovieDownloader(
+                    new Uri("http://openings.moe/"),
                     targetDirectory),
                 targetDirectory);
             DataContext = model;
@@ -63,7 +58,10 @@ namespace OpeningsMoeWpfClient
 
         private void OnPlayerClick(object sender, RoutedEventArgs e)
         {
-            OpeningPlayer.TogglePlayPause();
+            if(openingPlayer.IsPlaying)
+                openingPlayer.Pause();
+            else
+                openingPlayer.Resume();
         }
     }
 }
